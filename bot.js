6e8476e4eb12dba8fe99e7dcc8db95351e7f24c1
DombNexen <32490783+dombnexengr@users.noreply.github.com>
2020-12-07 08:13:33 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,32 @@
+  const TwitchJS = require('twitch-js')
+
+  const client = new TwitchJS.client({
+    channels: ['#dombnexen'],
+    identity: {
+      username: 'dombnexen',
+      password: 'oauth:n8lxszmmcl484lmm2a8s2exelj3ayr'
+    }
+  })
+
+  let loopInterval
+  client.on('chat', (channel, userstate, message, self) => {
+    console.log(`Message "${message}" received from ${userstate['display-name']}`)
+    if (self) return
+    const msg = message.split(' ')
+    if (msg[0].toLowerCase() === '$loop') {
+
+      if (loopInterval) { // Check if set
+        console.log('stop $loop')
+        clearInterval(loopInterval) // delete Timer
+        loopInterval = false
+      } else {
+        console.log('start $loop')
+        loopInterval = setInterval(function () {
+          client.say(channel, 'Test message') // client.say(channel, msg[1]) // ?
+        }, 30000) // 60000ms = 60s = 1min
+      }
+
+    }
+  })
+
+  client.connect()
